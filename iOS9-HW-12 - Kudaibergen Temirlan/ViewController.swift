8bf644aff212d430e6ec533194b6f1cdc2ad942b
temirlan <k.temirlan.t@gmail.com>
2023-02-26 13:50:17 +0600

    Initial Commit

---
+++ iOS9-HW-12 - Kudaibergen Temirlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS9-HW-12 - Kudaibergen Temirlan
+//
+//  Created by Темирлан Кудайберген on 26.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

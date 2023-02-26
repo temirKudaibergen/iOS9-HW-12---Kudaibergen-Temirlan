4b6500a8f48aa46cd2fa89b0b1d7e71831325162
temirlan <k.temirlan.t@gmail.com>
2023-02-27 00:09:18 +0600

    добавил секундомер с делением на минуты и секунды

--- iOS9-HW-12 - Kudaibergen Temirlan/ViewController.swift
+++ iOS9-HW-12 - Kudaibergen Temirlan/ViewController.swift
@@ -12,7 +12,7 @@ import SnapKit
 class ViewController: UIViewController {
     
     // MARK: - Outlets
-
+    
     private var shapeView: UIImageView = {
         let loadImage = UIImageView()
         loadImage.image = UIImage(named: "circle")
@@ -20,11 +20,8 @@ class ViewController: UIViewController {
         return loadImage
     }()
     
-    private var timerLable: UILabel = {
-        var isWorkTime = Bool()
-        var isStarted = Bool()
-        var timerLable = UILabel()
-        timerLable.text = ""
+    private var timerLable: UILabel = {        var timerLable = UILabel()
+        timerLable.text = " "
         timerLable.textColor = .black
         timerLable.textAlignment = .center
         timerLable.font = UIFont.boldSystemFont(ofSize: 84)
@@ -38,51 +35,88 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 20
         button.setImage(UIImage(named: "play"), for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Play", for: .normal)
-        button.setTitleColor(.black, for: .normal)
+        button.addTarget( .none, action: #selector(startTimer), for: .touchUpInside)
         return button
     }()
-
     
-
+    
+    
     // MARK: - Properties
     
     var timer = Timer()
     var durationTimer = 25
+    let shapLayer = CAShapeLayer()
     
     // MARK: - Lifcycle
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.animationCircular()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(shapeView)
-        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
-        timerLable.text = "\(durationTimer)"
-        shapeView.addSubview(timerLable)
-        shapeView.addSubview(startButton)
+        view.addSubview(timerLable)
+        view.addSubview(startButton)
+        startButton.addTarget(self,
+                              action: #selector(startTimer),
+                              for: .touchUpInside)
         view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png") ?? UIImage.remove)
         setupLayout()
         setupHierarchy()
-        
+        startTimer()
+        animationCircular()
     }
-     
-    @objc func startButtonTapped() {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+    
+    @objc func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(timerAction),
+                                     userInfo: nil, repeats: true)
     }
     
     @objc func timerAction() {
-        
         durationTimer -= 1
-        timerLable.text = "\(durationTimer)"
-        print(durationTimer)
+        timerLable.text = formatTime()
+        if durationTimer == 15 {
+            timer.invalidate()
+        }
     }
+    
+    func formatTime()->String{
+        let minutes = Int(durationTimer) / 60 % 60
+        let seconds = Int(durationTimer) % 60
+        return String(format:"%02i:%02i", minutes, seconds)
+        
+    }
+    
+    // MARK: - Animation
+    func animationCircular() {
+        let center = CGPoint(x: shapeView.frame.width / 2,
+                             y: shapeView.frame.height / 2)
+        let endAngle = (-CGFloat.pi / 2)
+        let startAngel = 2 * CGFloat.pi + endAngle
+        let circularPath = UIBezierPath(arcCenter: center,
+                                        radius: 120,
+                                        startAngle: startAngel,
+                                        endAngle: endAngle,
+                                        clockwise: true)
+        shapLayer.path = circularPath.cgPath
+        shapLayer.lineWidth = 21
+        shapLayer.fillColor = UIColor.clear.cgColor
+        shapLayer.strokeEnd = 1
+        shapLayer.lineCap = CAShapeLayerLineCap.round
+        shapLayer.strokeColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1).cgColor
+        shapeView.layer.addSublayer(shapLayer)
+    }
+    
     
     // MARK: - Setup
     
     private func setupHierarchy() {
         view.addSubview(shapeView)
-        shapeView.addSubview(timerLable)
-        shapeView.addSubview(startButton)
-        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        view.addSubview(timerLable)
+        view.addSubview(startButton)
     }
     
     private func setupLayout() {
@@ -97,10 +131,10 @@ class ViewController: UIViewController {
             make.centerY.equalTo(shapeView)
         }
         startButton.snp.makeConstraints{ make in
-            make.centerX.equalTo(shapeView)
-            make.centerY.equalTo(shapeView).offset(75)
-            make.height.equalTo(700)
-            make.width.equalTo(300)
+            make.centerX.equalTo(view)
+            make.centerY.equalTo(view).offset(75)
+            make.height.equalTo(50)
+            make.width.equalTo(50)
         }
     }
 }

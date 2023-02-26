56ef8348978a81e2336db8ee3b6053abef0a1bac
temirlan <k.temirlan.t@gmail.com>
2023-02-26 14:36:31 +0600

    Подготовка проекта

--- iOS9-HW-12 - Kudaibergen Temirlan/ViewController.swift
+++ iOS9-HW-12 - Kudaibergen Temirlan/ViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    MARK: - Outlets
+    
+    
+//    MARK: - Lifecyle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+//    MARK: - Setup
+    
+    
+//    MARK: - Actions
 
 
 }
